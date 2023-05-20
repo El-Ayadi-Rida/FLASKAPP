dffa6978fa981715b4296ec5cf3561ae5791b5a4
Rida El Ayadi <rdlzaar@gmail.com>
2023-05-21 00:12:41 +0100

    reda

---
+++ App.py
@@ -0,0 +1,33 @@
+from flask import Flask , render_template , Response
+import cv2
+App = Flask(__name__)
+
+camera=cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+
+@App.route('/')
+def index():
+    return render_template('index.html')
+
+@App.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__name__":
+    App.run(debug=True)
